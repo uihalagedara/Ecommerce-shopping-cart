2733eafcb045fa37f12cf5c17e6719079c32f7b5
Upendra Ihalagedara <78894119+uihalagedara@users.noreply.github.com>
2022-01-03 09:13:01 +0530

    Updated item getItems

--- redux/actions/itemActions.js
+++ redux/actions/itemActions.js
@@ -22,26 +22,7 @@ export const getItems = () => async (dispatch) =>{
     }
 };
 
-export const getItemDetails = (id) => async (dispatch) =>{
-    try {
-        dispatch({type:actionTypes.GET_ITEM_DETAILS_REQUEST});
-
-        const { data } = await axios.get(`/items/${id}`);
-
-        dispatch({
-            type: actionTypes.GET_ITEM_DETAILS_SUCCESS,
-            payload: data
-        });
-    } catch (error) {
-        dispatch({
-            type: actionTypes.GET_ITEM_DETAILS_FAIL,
-            payload:error.response && error.response.data.message 
-            ? error.response.data.message
-            :error.message,
-        });
-        
-    }
-};
+// getItemDetails to be implemented
 
 export const removeItemDetails = () => (dispatch) =>{
     dispatch({
