8dbcc1dee7ccfa7059ea493337215b227edda501
Upendra Ihalagedara <78894119+uihalagedara@users.noreply.github.com>
2022-01-03 09:11:42 +0530

    Updated cart action

--- redux/actions/cartActions.js
+++ redux/actions/cartActions.js
@@ -19,11 +19,4 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
 };
 
-export const removeFromCart = (id) => (dispatch, getState) => {
-  dispatch({
-    type: actionTypes.REMOVE_FROM_CART,
-    payload: id,
-  });
-
-  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
-};
+// removeFromCart to be implemented
